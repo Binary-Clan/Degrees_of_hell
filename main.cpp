e8b67c51c42658a504ce4e6239c9adfba5e6ff7e
Sudharaka <sudharakafernandobusiness@gmail.com>
2024-04-05 23:27:00 +0530

    Seed added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include "CDegrees.h"
 
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+#include <fstream> // For file input/output
+#include <iostream>
+
+using namespace std;
+
+// Function to read the seed value from "seed.txt"
+int readSeedFromFile(const char* filename) {
+    ifstream file(filename);
+    int seed = 0;
+    if (file.is_open()) {
+        file >> seed; // Read the seed from the file
+        file.close();
+    } else {
+        // If the file could not be opened, use the current time as a seed
+        cout << "Could not open seed file. Using current time as seed." << endl;
+        seed = static_cast<int>(time(nullptr));
+    }
+    return seed;
+}
+
+
 int main()
 {
+    int seed = readSeedFromFile("E:\\Projects\\Degrees of Hell\\seed.txt");
+
+    srand(seed);
     CDegrees game;
 
     game.ReadSpaces("degrees.txt");
@@ -14,3 +40,5 @@ int main()
 
     return 0;
 }
+
+
